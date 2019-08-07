4f189a8b93f83e5d7ae7415499ca594ddbd3350b
Joel Felipe Gomes dos Santos <joelfelipe338@gmail.com>
2019-08-07 10:14:49 -0300

    adicionando pasta do projeto em c++

---
+++ jogo da velha c++/jogo da velha.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout << "digite um numero" << endl;
+    cin >> x;
+    cout << "vc digitou " << x << endl;
+    return 0;
+}
