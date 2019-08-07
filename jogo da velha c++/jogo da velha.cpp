9cead197bfde05d7806e9b624d009645bfbb0714
JOEL FELIPE GOMES DOS SANTOS <joelfelipe338@A202-09L.imd.local>
2019-08-07 16:54:48 -0300

    um <-> 1

--- jogo da velha c++/jogo da velha.cpp
+++ jogo da velha c++/jogo da velha.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     int x;
-    cout << "digite um numero" << endl;
+    cout << "digite 1 numero" << endl;
     cin >> x;
     cout << "vc digitou " << x << endl;
     return 0;
